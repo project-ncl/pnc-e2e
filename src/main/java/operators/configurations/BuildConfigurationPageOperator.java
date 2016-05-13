4760fe686efadab9619a85a99d2507bd7a333e5f
Pavel Kralik <pkralik@redhat.com>
2016-05-13 12:18:04 +0200

    Adding Required field Project

--- src/main/java/operators/configurations/BuildConfigurationPageOperator.java
+++ src/main/java/operators/configurations/BuildConfigurationPageOperator.java
@@ -16,24 +16,23 @@ public class BuildConfigurationPageOperator extends Operator {
     }
 
     public void createBuildConfiguration() {
-        new LinkOperator(Elements.BUILD_CONFIGURATION_LINK).clickLink();
+        new LinkOperator(Elements.CONFIGURATION_LINK).clickLink();
         new LinkOperator(Elements.BUILD_CONFIGURATION_LINK).findLink(1).click(); //second link must be clicked since they have the same name.
         new ButtonOperator(Elements.CREATE_CONFIGURATION_BUTTON).clickButton();
+        new SelectOperator(Elements.BUILD_CONFIGURATION_PROJECT_SELECT).clickSelect(1);
         new TextInputOperator(Elements.BUILD_CONFIGURATION_INPUT).insertInput(name);
-        //new SelectOperator(Elements.BUILD_CONFIGURATION_PROJECT_SELECT).clickSelect(2);
         new AreaTextOperator(Elements.BUILD_CONFIGURATION_DESCRIPTION).textAreaInput(Strings.BUILD_CONFIGURATION_DESCRIPTION);
         new TextInputOperator(Elements.BUILD_CONFIGURATION_SCM_URL).insertInput(Strings.BUILD_CONFIGURATION_SCM_URL);
         new TextInputOperator(Elements.BUILD_CONFIGURATION_SCM_REVISION).insertInput(Strings.BUILD_CONFIGURATION_SCM_REVISION);
         new AreaTextOperator(Elements.BUILD_CONFIGURATION_BUILD_SCRIPT).textAreaInput(Strings.BUILD_CONFIGURATION_BUILD_SCRIPT);
-        new TextInputOperator(Elements.BUILD_CONFIGURATION_ENVIRONMENT).insertInput("");
-        driver.findElements(By.tagName("li")).get(3).click();
+        new SelectOperator(Elements.BUILD_CONFIGURATION_ENVIRONMENT).clickSelect(1);
         //new TextInputOperator(Elements.BUILD_CONFIGURATION_PRODUCT).insertInputEnter(Strings.BUILD_CONFIGURATION_VERSION);
         new SubmitOperator().submit();
     }
 
     public void buildBuildConfiguration() {
         new LinkOperator(Elements.CONFIGURATION_LINK).clickLink();
-        new LinkOperator(Elements.BUILD_CONFIGURATION_LINK).clickLink();
+        new LinkOperator(Elements.BUILD_CONFIGURATION_LINK).findLink(1).click();
         new LinkOperator(name).clickLink();
         new BuildOperator().startBuild();
     }
