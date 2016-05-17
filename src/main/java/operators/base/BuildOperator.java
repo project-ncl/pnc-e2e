2088bec9764c442289ab60d2c7b4ff8201d512fa
Pavel Kralik <pkralik@redhat.com>
2016-05-17 16:39:44 +0200

    Waiting 3 seconds to tooltip relocates and Build button becomes visible

--- src/main/java/operators/base/BuildOperator.java
+++ src/main/java/operators/base/BuildOperator.java
@@ -2,13 +2,21 @@ package operators.base;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 /**
  * Created by eunderhi on 06/10/15.
  */
 public class BuildOperator extends Operator {
     public void startBuild() {
-        WebElement startButton = driver.findElement(By.xpath("//button[contains(@ng-click, 'build()')]"));
+        // Alternative locator By.xpath("//button[contains(@ng-click, 'build()')]")
+        WebElement startButton = driver.findElement(By.xpath("//span[text() = 'Build'] "));
+        Actions action = new Actions(driver);
+        action.moveToElement(startButton).build().perform();
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+        }
         startButton.click();
     }
 }
