bd99e459dc06595bfd394b01ec631ed51b2ad926
Pavel Kralik <pkralik@redhat.com>
2016-05-18 17:14:18 +0200

    Fixing search by partial text

--- src/test/java/ui/UITest.java
+++ src/test/java/ui/UITest.java
@@ -67,7 +67,7 @@ public class UITest {
     public void assertCloneExists(String configurationName) {
         String cloneName = "_" + configurationName;
         try {
-            tester.getDriver().findElement(By.linkText(cloneName)).isDisplayed();
+            tester.getDriver().findElement(By.partialLinkText(cloneName)).isDisplayed();
         }
         catch (NoSuchElementException e) {
             throw new AssertionFailedError("Cloned configuration " + cloneName + " does not exist when it should");
