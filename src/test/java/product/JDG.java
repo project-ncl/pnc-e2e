bb1bb8b3aba7d57b0806450e4018cbae9d7028c2
Pavel Kralik <pkralik@redhat.com>
2016-05-27 17:00:21 +0200

    Adding the JDG Build Group Config

---
+++ src/test/java/product/JDG.java
@@ -0,0 +1,102 @@
+package product;
+
+import operators.configurations.BuildConfigurationPageOperator;
+import operators.configurations.BuildConfigurationSetPageOperator;
+import operators.projects.ProjectPageOperator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ui.UITest;
+import util.RandomName;
+
+/**
+ * Created by pkralik
+ */
+public class JDG extends UITest {
+
+    private BuildConfigurationSetPageOperator buildGroupConfig;
+    private String buildName;
+    private String sufix;
+
+    @Before
+    public void createGroupConfig() {
+
+        sufix = RandomName.getSufix();
+        // Build Group Config
+        buildName = "jdg" + sufix;
+        buildGroupConfig = new BuildConfigurationSetPageOperator(buildName);
+        buildGroupConfig.createBuildGroupConfig();
+        assertLinkExists(buildName);
+    }
+
+    @Test
+    public void createKeycloak() {
+
+        // infinispan
+        String infinispanProject = "jdg-infinispan";
+        String infinispanName = infinispanProject + sufix;
+        new ProjectPageOperator(infinispanProject).createProject("JDG Infinispan");
+        BuildConfigurationPageOperator config = new BuildConfigurationPageOperator(infinispanName);
+        config.createBuildConfig();
+        config.setProject(infinispanProject);
+        config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/infinispan.git");
+        config.setScmRevision("JDG_7.0.0.ER4_pnc_wa_2");
+        config.setBuildScript("# mvn dependency:tree > deptree\nsleep 6000\n# mvn -X clean deploy -DskipTests");
+        config.setDefaultConfigEnvironment();
+        config.setBuildConfigGroup(buildName);
+        config.submit();
+
+        // spark
+        String sparkProject = "jdg-spark";
+        String sparkName = sparkProject + sufix;
+        new ProjectPageOperator(sparkProject).createProject("JDG Spark");
+        config = new BuildConfigurationPageOperator(sparkName);
+        config.createBuildConfig();
+        config.setProject(sparkProject);
+        config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/jdg-spark.git");
+        config.setScmRevision("JDG_7.0.0.ER4");
+        config.setBuildScript("mvn clean deploy -DskipTests");
+        config.setDefaultConfigEnvironment();
+        config.setDependencies(infinispanName);
+        config.setBuildConfigGroup(buildName);
+        config.submit();
+
+        // camel
+        String camelProject = "jdg-camel";
+        String camelName = camelProject + sufix;
+        new ProjectPageOperator(camelProject).createProject("JDG Camel");
+        config = new BuildConfigurationPageOperator(camelName);
+        config.createBuildConfig();
+        config.setProject(camelProject);
+        config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/jdg-camel.git");
+        config.setScmRevision("JDG_7.0.0.ER4");
+        config.setBuildScript("mvn clean javadoc:javadoc deploy -DskipTests");
+        config.setDefaultConfigEnvironment();
+        config.setDependencies(infinispanName);
+        config.setBuildConfigGroup(buildName);
+        config.submit();
+
+        // packaging
+        String packagingProject = "jdg-packaging";
+        String packagingName = packagingProject + sufix;
+        new ProjectPageOperator(packagingProject).createProject("JDG Packaging");
+        config = new BuildConfigurationPageOperator(packagingName);
+        config.createBuildConfig();
+        config.setProject(packagingProject);
+        config.setScmUrl("http://git.app.eng.bos.redhat.com/jdg-packaging.git");
+        config.setScmRevision("JDG_7.0.0.ER4");
+        config.setBuildScript("mvn clean deploy -DskipTests -Pdistribution");
+        config.setDefaultConfigEnvironment();
+        config.setDependencies(infinispanName, camelName);
+        config.setBuildConfigGroup(buildName);
+        config.submit();
+    }
+
+    @After
+    public void buildGroupConfig() {
+
+        buildGroupConfig.buildBuildConfigurationSet();
+        buildGroupConfig.menuBuildGroups();
+        assertLinkExists(buildName);
+    }
+}
