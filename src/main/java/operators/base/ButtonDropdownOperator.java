e6b57fee8762ed82e7bbf28e58230d5ba84723d1
Pavel Kralik <pkralik@redhat.com>
2016-06-02 17:00:12 +0200

    Button dropdown input added.

---
+++ src/main/java/operators/base/ButtonDropdownOperator.java
@@ -0,0 +1,34 @@
+package operators.base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by pkralik
+ */
+public class ButtonDropdownOperator extends Operator {
+
+    public final String XPATH_SELECT = "//*[@selected-id='%s']/div";
+
+    public ButtonDropdownOperator(String dropdown) {
+
+        super(dropdown);
+    }
+
+    public void clickSelect(String value) {
+
+        String selectXpath = String.format(XPATH_SELECT, name);
+        WebElement element = getElementByXpath(selectXpath);
+        element.findElement(By.tagName("button")).click();
+        element.findElement(By.linkText(value)).click();
+    }
+
+    public void clickFirst() {
+
+        String selectXpath = String.format(XPATH_SELECT, name);
+        WebElement element = getElementByXpath(selectXpath);
+        element.findElement(By.tagName("button")).click();
+        element.findElements(By.tagName("li")).get(0).click();
+    }
+}
