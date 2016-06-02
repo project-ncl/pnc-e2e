ad108cfec13161dd4d1c555a0eef1acfc76db10f
Pavel Kralik <pkralik@redhat.com>
2016-06-02 14:52:30 +0200

    Test for importing Product.

--- src/test/java/product/ImportProductTest.java
+++ src/test/java/product/ImportProductTest.java
@@ -1,8 +1,8 @@
 package product;
 
-import operators.products.VersionPageOperator;
 import operators.products.ProductPageOperator;
 import operators.projects.ProjectPageOperator;
+import operators.products.ImportPageOperator;
 import org.junit.Test;
 import ui.UITest;
 
@@ -18,8 +18,12 @@ public class ImportProductTest extends UITest {
         final String productName = "pnc-simple-test";
         final String productVersion = "1.0";
         new ProductPageOperator(productName).createProduct("PNC Simple Test product");
-        new VersionPageOperator(productName).createVersion(productVersion);
-        assertLinkExists(productVersion);
+        //new VersionPageOperator(productName).createVersion(productVersion);
+        //assertLinkExists(productVersion);
         new ProjectPageOperator(productName).createProject("PNC Simple Test project");
+
+        ImportPageOperator product = new ImportPageOperator(productName);
+        product.importProduct(productVersion, "https://github.com/project-ncl/pnc-simple-test-project.git", "master");
+        product.finishImport("mvn javadoc:jar deploy");
     }
 }
