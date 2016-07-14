15d4286a46cc480b5ad2908c37345e5d4c42ad5e
Pavel Kralik <pkralik@redhat.com>
2016-07-14 11:18:56 +0200

    Update for Import/Config Set of JDG/SSO/EAP

--- src/test/java/product/BuildGroupConfigTest.java
+++ src/test/java/product/BuildGroupConfigTest.java
@@ -40,7 +40,7 @@ public class BuildGroupConfigTest extends UITest {
         config.createBuildConfig();
         config.setProject(consoleProject);
         config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/jdg-management-console.git");
-        config.setScmRevision("JDG_7.0.0.ER9");
+        config.setScmRevision("JDG_7.0.0.ER4_pnc_wa__4");
         config.setBuildScript("export NVM_NODEJS_ORG_MIRROR=http://rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging/jboss-dg/node\n\n"
                 + "mvn clean deploy "
                 + "-DnpmDownloadRoot=http://rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging/jboss-dg/node/npm/ "
@@ -58,7 +58,7 @@ public class BuildGroupConfigTest extends UITest {
         config.createBuildConfig();
         config.setProject(infinispanProject);
         config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/infinispan.git");
-        config.setScmRevision("JDG_7.0.0.ER9");
+        config.setScmRevision("JDG_7.0.0.ER4_pnc_wa_5");
         config.setBuildScript("mvn clean deploy -DskipTests -Pdistribution");
         config.setDefaultConfigEnvironment();
         config.setDependencies(consoleName);
@@ -73,7 +73,7 @@ public class BuildGroupConfigTest extends UITest {
         config.createBuildConfig();
         config.setProject(sparkProject);
         config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/jdg-spark.git");
-        config.setScmRevision("JDG_7.0.0.ER9");
+        config.setScmRevision("JDG_7.0.0.ER4");
         config.setBuildScript("mvn clean deploy -DskipTests");
         config.setDefaultConfigEnvironment();
         config.setDependencies(infinispanName);
@@ -88,7 +88,7 @@ public class BuildGroupConfigTest extends UITest {
         config.createBuildConfig();
         config.setProject(camelProject);
         config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/jdg-camel.git");
-        config.setScmRevision("JDG_7.0.0.ER9");
+        config.setScmRevision("JDG_7.0.0.ER4_pnc_wa_1");
         config.setBuildScript("mvn clean javadoc:javadoc deploy -DskipTests");
         config.setDefaultConfigEnvironment();
         config.setDependencies(infinispanName);
@@ -103,7 +103,7 @@ public class BuildGroupConfigTest extends UITest {
         config.createBuildConfig();
         config.setProject(packagingProject);
         config.setScmUrl("http://git.app.eng.bos.redhat.com/jdg-packaging.git");
-        config.setScmRevision("JDG_7.0.0.ER9");
+        config.setScmRevision("JDG_7.0.0.ER4");
         config.setBuildScript("mvn clean deploy -DskipTests -Pdistribution");
         config.setDefaultConfigEnvironment();
         config.setDependencies(infinispanName, camelName);
@@ -118,7 +118,7 @@ public class BuildGroupConfigTest extends UITest {
         config.createBuildConfig();
         config.setProject(hadoopProject);
         config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/jdg-hadoop.git");
-        config.setScmRevision("JDG_7.0.0.ER9");
+        config.setScmRevision("JDG_7.0.0.ER4");
         config.setBuildScript("mvn clean deploy -DskipTests");
         config.setDefaultConfigEnvironment();
         config.setDependencies(infinispanName);
@@ -133,7 +133,7 @@ public class BuildGroupConfigTest extends UITest {
         config.createBuildConfig();
         config.setProject(cassandraProject);
         config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/jdg-cachestore-cassandra.git");
-        config.setScmRevision("JDG_7.0.0.ER9");
+        config.setScmRevision("JDG_7.0.0.ER4");
         config.setBuildScript("mvn clean deploy -DskipTests");
         config.setDefaultConfigEnvironment();
         config.setDependencies(infinispanName);
@@ -148,7 +148,7 @@ public class BuildGroupConfigTest extends UITest {
         config.createBuildConfig();
         config.setProject(archetypeProject);
         config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/jdg-cachestore-archetype.git");
-        config.setScmRevision("JDG_7.0.0.ER9");
+        config.setScmRevision("JDG_7.0.0.ER4");
         config.setBuildScript("mvn clean deploy -DskipTests");
         config.setDefaultConfigEnvironment();
         config.setDependencies(infinispanName);
@@ -163,7 +163,7 @@ public class BuildGroupConfigTest extends UITest {
         config.createBuildConfig();
         config.setProject(jsClientProject);
         config.setScmUrl("http://git.app.eng.bos.redhat.com/infinispan/jdg-js-client.git");
-        config.setScmRevision("JDG_7.0.0.ER9");
+        config.setScmRevision("JDG_7.0.0.ER4");
         config.setBuildScript("export NVM_NODEJS_ORG_MIRROR=http://rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging/jboss-dg/node\n\n"
                 + "mvn clean deploy "
                 + "-DnpmDownloadRoot=http://rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging/jboss-dg/node/npm/ "
@@ -220,9 +220,7 @@ public class BuildGroupConfigTest extends UITest {
         config.setProject(zxingProject);
         config.setScmUrl("https://github.com/zxing/zxing.git");
         config.setScmRevision("zxing-3.2.1");
-        config.setBuildScript("mvn clean deploy -DskipTests -Drat.numUnapprovedLicenses=2 "
-                + "-Dplugin-removal=org.codehaus.mojo:clirr-maven-plugin "
-                + "-DdependencyExclusion.com.beust:jcommander@*=1.48.0.redhat-1");
+        config.setBuildScript("mvn clean deploy -DskipTests -Drat.numUnapprovedLicenses=2");
         config.setDefaultConfigEnvironment();
         config.setBuildConfigGroup(buildName);
         config.submit();
@@ -230,39 +228,15 @@ public class BuildGroupConfigTest extends UITest {
         // Keycloak
         String keycloakProject = "keycloak";
         new ProjectPageOperator(keycloakProject).createProject("Keycloak project");
-        String keycloakName = "keycloak-parent-1.9.x-redhat" + sufix;
+        String keycloakName = "keycloak-parent-1.9.0.Final-redhat" + sufix;
         config = new BuildConfigurationPageOperator(keycloakName);
         config.createBuildConfig();
         config.setProject(keycloakProject);
         config.setScmUrl("http://git.engineering.redhat.com/git/users/pkralik/keycloak-prod.git");
-        config.setScmRevision("2.0.x-redhat");
-        config.setBuildScript("mvn clean deploy -Pdistribution "
-                + "-pl '!adapters/oidc/jetty/jetty9.1' -pl '!adapters/oidc/jetty/jetty9.2' -pl '!adapters/oidc/spring-boot' -pl '!adapters/oidc/spring-security' -pl '!adapters/oidc/tomcat/tomcat6' -pl '!adapters/oidc/tomcat/tomcat7' -pl '!adapters/oidc/tomcat/tomcat8' -pl '!adapters/oidc/wildfly/wf8-subsystem' -pl '!adapters/saml/jetty/jetty8.1' -pl '!adapters/saml/jetty/jetty9.1' -pl '!adapters/saml/jetty/jetty9.2' -pl '!adapters/saml/tomcat/tomcat6' -pl '!adapters/saml/tomcat/tomcat7' -pl '!adapters/saml/tomcat/tomcat8' -pl '!distribution/adapters/as7-eap6-adapter/as7-adapter-zip' -pl '!distribution/adapters/tomcat6-adapter-zip' -pl '!distribution/adapters/tomcat7-adapter-zip' -pl '!distribution/adapters/tomcat8-adapter-zip' -pl '!distribution/adapters/jetty81-adapter-zip' -pl '!distribution/adapters/jetty91-adapter-zip' -pl '!distribution/adapters/jetty92-adapter-zip' -pl '!distribution/adapters/wf8-adapter/wf8-adapter-zip' -pl '!distribution/adapters/wf8-adapter/wf8-modules' -pl '!distribution/api-docs-dist' -pl '!distribution/feature-packs/adapter-feature-pack' -pl '!distribution/demo-dist' -pl '!distribution/examples-dist' -pl '!distribution/proxy-dist' -pl '!distribution/saml-adapters/as7-eap6-adapter/as7-adapter-zip' -pl '!distribution/saml-adapters/tomcat6-adapter-zip' -pl '!distribution/saml-adapters/tomcat7-adapter-zip' -pl '!distribution/saml-adapters/tomcat8-adapter-zip' -pl '!distribution/saml-adapters/jetty81-adapter-zip' -pl '!distribution/saml-adapters/jetty92-adapter-zip' -pl '!distribution/src-dist' -pl '!model/mongo' -pl '!proxy/proxy-server' -pl '!proxy/launcher/' -pl '!testsuite/proxy' -pl '!testsuite/tomcat6' -pl '!testsuite//tomcat7' -pl '!testsuite/tomcat8' -pl '!testsuite/jetty/jetty81' -pl '!testsuite/jetty/jetty91' -pl '!testsuite/jetty/jetty92' -pl '!testsuite/stress'");
+        config.setScmRevision("1.9.0.Final-redhat");
+        config.setBuildScript("mvn clean deploy -Pdistribution");
         config.setDefaultConfigEnvironment();
         config.setDependencies(liquibaseName, twitter4jName, zxingName);
-        config.setBuildConfigGroup(buildName);
-        config.submit();
-    }
-
-    @Test
-    public void eap() {
-
-        // Build Group Config
-        buildName = "eap7" + sufix;
-        buildGroupConfig = new BuildConfigurationSetPageOperator(buildName);
-        buildGroupConfig.createBuildGroupConfig();
-
-        // EAP7
-        String keycloakProject = "eap7";
-        new ProjectPageOperator(keycloakProject).createProject("JBoss EAP 7 project");
-        String keycloakName = "jboss-eap-parent-7.0.1.GA-redhat" + sufix;
-        BuildConfigurationPageOperator config = new BuildConfigurationPageOperator(keycloakName);
-        config.createBuildConfig();
-        config.setProject(keycloakProject);
-        config.setScmUrl("http://git.engineering.redhat.com/git/users/pkralik/wildfly.git");
-        config.setScmRevision("eap-7.0.x");
-        config.setBuildScript("mvn clean deploy -Prelease");
-        config.setDefaultConfigEnvironment();
         config.setBuildConfigGroup(buildName);
         config.submit();
     }
